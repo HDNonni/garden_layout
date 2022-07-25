727021194d8b6f2bf95d14382e6982a1ff8b8b5e
Carol <27028067+HDNonni@users.noreply.github.com>
2022-07-24 20:09:48 -0400

    Deploy

---
+++ precache-manifest.3d386020b8d50d13e9fd7cb4f7a9b89f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edce18cb509c63baa6339a9985bd67ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "31737766695b87cac2ca",
+    "url": "/static/css/main.2727e664.chunk.css"
+  },
+  {
+    "revision": "6292cd6ada14310e7314",
+    "url": "/static/js/2.4c263aac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4c263aac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31737766695b87cac2ca",
+    "url": "/static/js/main.ca3ad026.chunk.js"
+  },
+  {
+    "revision": "34977fcb6250f7dde15f",
+    "url": "/static/js/runtime-main.45be8752.js"
+  },
+  {
+    "revision": "1586768737099f52bc14a9323673c5b7",
+    "url": "/static/media/woods_garden.15867687.jpg"
+  }
+]);
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,31 +1,64 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import woodsGarden from "../assets/images/woods_garden.jpg";
+import "./Main.css";
 
-function Main() {
-    //define a useState hook
-  const [data, setData] = useState({ flowers: [] });
-//use Effect executes code in block 
+function Main(props) {
+  //define a useState hook
+  const [data, setData] = useState({ flower: [] });
+  //use Effect executes code in block
   useEffect(() => {
-      //https request with promises
-    axios("http://localhost:3000/list").then(function (result) {
+    //https request with promises
+    axios("http://localhost:8081/flowers").then(function (result) {
       setData(result.data);
     });
   }, []);
+  console.log(data);
+//goal : click on a flower using html map and display its information from the api
+  let flowers = data;
+  const [clickedFlowerName, setValue] = useState("");
+//add useEffect but needs to wait for click
+  // useEffect(() => {
+  //   setValue("clicked flower")
+  //   console.log(clickedFlowerName)
+  // },[setValue,clickedFlowerName])
+ 
+  // const [name, setName] = useState({flowers: []});
+
+  let handleClick = (event) => {
+    setValue(event.target.alt)
+    console.log(event.target.alt)
+  }
+  
   return (
     <div>
-        {/* rendering an array of items */}
-      {data.flowers.map(item => (
-        <p >
-          {item.name} 
-        </p>
-       ) )}
+      <img
+        src={woodsGarden}
+        alt="woods garden"
+        useMap="#woodsmap"
+        width="800"
+        height="400"
+      ></img>
+      <map name="woodsmap">
+        <area shape="circle" coords="75,75,75" alt = "bleeding heart" onClick={handleClick}></area>
+        <area shape="circle" coords="175,75,75"  alt = "candy tuft"></area>
+      </map>
+      <div className="clickedFlower" >{clickedFlowerName}</div>
+     
+     
       <ul>
-
-        <li>Black-eye Susan</li>
-        <li>Echinechea</li>
-        <li>Lilac</li>
-        <li>Phlox</li>
-        <li>Lilies</li>
+        {
+        data.flower.map((clickedFlowerName) => (
+          <li key={clickedFlowerName}>
+            {/* Flower name: */}
+            <div>
+              
+            </div>
+            <div>Flower name: {clickedFlowerName.name}</div>
+            <div>Blooms in: {clickedFlowerName.bloomtime}</div>
+          </li>
+          
+        ))}
       </ul>
     </div>
   );
